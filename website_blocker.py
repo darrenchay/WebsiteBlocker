2cdadfbb84b1f8070b2517dbd5675a2432f1bf15
darrenchay <darrenchay@gmail.com>
2020-04-27 10:36:43 -0400

    added exit handler

--- website_blocker.py
+++ website_blocker.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python
 import time
 from win10toast import ToastNotifier
+import atexit
+
 host_path = r"C:/Windows/System32/drivers/etc/hosts"
 local_host = "127.0.0.1"
 
 websites = ["www.facebook.com", "www.youtube.com", "facebook.com", "youtube.com", "www.github.com", "github.com"]
+
+
+def exit_handler():
+    with open(host_path, "r+") as curr_file:
+        file_content = curr_file.readlines()
+        curr_file.seek(0)
+        for line in file_content:
+            if not any(curr_website in line for curr_website in websites):
+                curr_file.write(line)
+
+            curr_file.truncate()
+
+    toast = ToastNotifier()
+    toast.show_toast("Website Blocker", "Terminating Website Blocker...", duration=2)
+
 
 try:
     while True:
@@ -18,15 +35,6 @@ try:
                     file.write(local_host + " " + website + "\n")
         time.sleep(5)
 
-except KeyboardInterrupt:
-    with open(host_path, "r+") as file:
-        content = file.readlines()
-        file.seek(0)
-        for line in content:
-            if not any(website in line for website in websites):
-                file.write(line)
+finally:
+    exit_handler()
 
-            file.truncate()
-
-    toast = ToastNotifier()
-    toast.show_toast("Website Blocker", "Terminating Website Blocker...", duration=2)
